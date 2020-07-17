bb29ef94616548b2e1024f67d7fda9218a7ed14b
shawnAshton <39253954+shawnAshton@users.noreply.github.com>
2020-07-16 18:04:20 -0600

    Add files via upload
    The newest version of the code!

--- TempSensorWindowOpener/TempSensorWindowOpener.ino
+++ TempSensorWindowOpener/TempSensorWindowOpener.ino
@@ -7,17 +7,23 @@
 #define NOTE_G5  784
 #define NOTE_C6  1047
 
+const int rs = A4, en = A5, d4 = A0, d5 = A1, d6 = A2, d7 = A3;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 250;
+
 /*SETTINGS*/
 int preferredTemperature = 68;
+bool justChangedPrefTemp = false;
 
 //pins
 const int TempInPin = 52;
 const int TempOutPin = 22;
-const int magnetPin = 40;
+const int magnetPin = 13;
 const int alarmPin = 50;
 
 //minutes between temperature readings
-const int mins = 15;
+const int mins = 5;
 
 
 //use an array of ints and play melody of ascending notes
@@ -25,17 +31,23 @@ void playSoundOpen()
 {
   int Sound[] = {NOTE_G4, NOTE_C5, NOTE_G5, NOTE_C6};
   int SoundNoteDurations[] = {12, 8, 6, 4};
-  playMelody(Sound, SoundNoteDurations, 4);
+  playMelody(Sound, SoundNoteDurations, 4, 2);
 
 }
 
+void playSoundChangePref()
+{
+  int Sound[] = {NOTE_G4,NOTE_G4};
+  int SoundNoteDurations[] = {20, 20};
+  playMelody(Sound, SoundNoteDurations, 2, 1);
+}
 
 //use an array of ints and play melody of descending notes
 void playSoundClose()
 {
   int Sound[] = {NOTE_C6, NOTE_G5, NOTE_C5, NOTE_G4};
   int SoundNoteDurations[] = {12, 8, 6, 4};
-  playMelody(Sound, SoundNoteDurations, 4);
+  playMelody(Sound, SoundNoteDurations, 4, 2);
 }
 
 
@@ -143,6 +155,15 @@ bool ShouldWindowClose(int tempInside, int tempOutside)
 
 void setup()
 {
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(2), increaseTemp, RISING);
+  attachInterrupt(digitalPinToInterrupt(3), decreaseTemp, RISING);
+    // set up the LCD's number of columns and rows:
+  lcd.begin(20, 4);
+  // Print a message to the LCD.
+  lcd.print("Temperature Project");
+  
   //setup the dht sensors
   dhtIndoor.begin();
   dhtOutdoor.begin();
@@ -169,7 +190,24 @@ void loop() {
   float outdoorHI = dhtOutdoor.computeHeatIndex(outdoorTempF, outdoorH);
 
   
+   lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print("In Temp:  ");
+  lcd.print(indoorTempF);
+  lcd.print((char)223);
+  lcd.print("F");
   
+  lcd.setCursor(0, 2);
+  lcd.print("Out Temp: ");
+  lcd.print(outdoorTempF);
+  lcd.print((char)223);
+  lcd.print("F");
+  
+  lcd.setCursor(0, 3);
+  lcd.print("Our Temp: ");
+  lcd.print(preferredTemperature);
+  lcd.print((char)223);
+  lcd.print("F");
   if(ShouldWindowClose(indoorHI, outdoorHI))
   {
     //notify the user to close the window
@@ -181,20 +219,53 @@ void loop() {
     playSoundOpen();
   }
 
- delay(mins*60*1000);
+
+for(int i = 0; i < mins*60 * 250; i++)
+{
+   delay(30);
+   if(justChangedPrefTemp)
+  {
+    justChangedPrefTemp = false;
+    lcd.setCursor(10, 3);
+    lcd.print(preferredTemperature);
+    playSoundChangePref();
+  }
+}
+
+ 
   
 }
 
-void playMelody(int *melody, int *noteDurations, int notesLength)
+void playMelody(int *melody, int *noteDurations, int notesLength, int reps)
 {
-for(int num = 0; num < 10; num++)
-{
-  for (int thisNote = 0; thisNote < notesLength; thisNote++) {
-    int noteDuration = 1000 / noteDurations[thisNote];
-    tone(alarmPin, melody[thisNote], noteDuration);
-    int pauseBetweenNotes = noteDuration * 1.30;
-    delay(pauseBetweenNotes);
-    noTone(alarmPin);
+  for(int num = 0; num < reps; num++)
+  {
+    for (int thisNote = 0; thisNote < notesLength; thisNote++) {
+      int noteDuration = 2000 / noteDurations[thisNote];
+      tone(alarmPin, melody[thisNote], noteDuration);
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+      noTone(alarmPin);
+    }
   }
 }
+
+void increaseTemp()
+{
+  if((millis() - lastDebounceTime) > debounceDelay)
+  {
+    lastDebounceTime = millis();
+    preferredTemperature++;
+    justChangedPrefTemp = true;
+  }
+}
+
+void decreaseTemp()
+{
+  if((millis() - lastDebounceTime) > debounceDelay)
+  {
+    lastDebounceTime = millis();
+    preferredTemperature--;
+    justChangedPrefTemp = true;
+  }
 }
